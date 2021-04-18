8fdfbf663b65bb726ea82d2be00fdec1a88ab2f6
Deepak Raj <54245038+codePerfectPlus@users.noreply.github.com>
2021-04-18 11:03:11 +0530

    request to aiohttp changed

--- src/app.py
+++ src/app.py
@@ -9,22 +9,14 @@ Invite-Link:-
 https://discord.com/api/oauth2/authorize?client_id=832137823309004800&permissions=268446835&scope=bot
 
 """
-import os
+import re
 import random
 import discord
 from discord import Intents
 from discord.ext import commands
-import requests
-from dotenv import load_dotenv
+import aiohttp
 
 import utils
-
-load_dotenv()
-print(f'Discord Version : {discord.__version__}')
-
-# environment variables 
-TOKEN = os.environ.get('DISCORD_TOKEN')
-WEATHER_API_KEY = os.environ.get('WEATHER_API_KEY')
 
 intents = Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
@@ -32,127 +24,146 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    """ Change Bot Presense """
-    await bot.change_presence(activity=discord.Game(name="Fornite"))
-    print(f'{bot.user.name} is Online...')
+	""" Change Bot Presense """
+	await bot.change_presence(activity=discord.Game(name="Fornite"))
+	print(f'{bot.user.name} is Online...')
 
 
 @bot.event
-async def on_message(message):
-    """ some on_message command """
-    if message.author.id == bot.user.id:
-        return
-    msg_content = message.content.lower()
+async def on_message(message: str):
+	""" on_message command """
+	if message.author.id == bot.user.id:
+		return
+	msg_content = message.content.lower()
 
-    if msg_content.startswith('ping'):
-        await message.channel.send("Pong")
-    if msg_content.startswith('pong'):
-        await message.channel.send("Ping")
-    
-    curse_words = ["fuck"]
-    if any(word in msg_content for word in curse_words):
-        await message.delete()
-        
-    await bot.process_commands(message)
+	if msg_content.startswith('ping'):
+		await message.channel.send("Pong")
+	if msg_content.startswith('pong'):
+		await message.channel.send("Ping")
+
+	await bot.process_commands(message)
 
 
 @bot.command(name="quote", help="get amazing random quote")
-async def quote(ctx):
-    """ Get amazing random quote """
-    randomQuote = utils._getRandomQuote()
-    await ctx.send(randomQuote)
+async def getRandomQuote(ctx):
+	""" Get amazing random quote """
+	randomQuoteURL = 'https://zenquotes.io/api/random'
+	async with ctx.typing():
+		async with aiohttp.ClientSession() as session:
+			async with session.get(randomQuoteURL) as response:
+				result = await response.json()
+				randomQuote = f'{result[0]["q"]} -**{result[0]["a"]}**'
+				await ctx.send(randomQuote)
 
 
 @bot.command(name="joke", help="get random jokes")
-async def joke(ctx):
-    """ Get random jokes """
-    randomJoke = utils._getRanomJoke()
-    await ctx.send(randomJoke)
+async def getRandomJoke(ctx):
+	""" Get random jokes """
+	randomJokeURL = 'https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single'
+	async with ctx.typing():
+		async with aiohttp.ClientSession() as session:
+			async with session.get(randomJokeURL) as response:
+				randomJoke = await response.json()["joke"]
+				await ctx.send(randomJoke)
 
 
 @bot.command(name="fact", help="get amazing random fact")
-async def fact(ctx):
-    """ Get amazing random fact """
-    randomFact = utils._getRandomFact()
-    await ctx.send(randomFact)
+async def getRandomFact(ctx):
+	""" Get amazing random fact """
+	randomFactURL = 'https://uselessfacts.jsph.pl//random.json?language=en'
+	async with ctx.typing():
+		async with aiohttp.ClientSession() as session:
+			async with session.get(randomFactURL) as response:
+				randomFact = await response.json()['text']
+				await ctx.send(randomFact)
+
+
+@bot.command(name="cats", help="Get Random Pictures of Cats")
+async def getRandomcCatPicture(ctx):
+	""" Get Random Cats Picture """
+	catAPIURL = "https://thatcopy.pw/catapi/rest/"
+	async with ctx.typing():
+		async with aiohttp.ClientSession() as session:
+			async with session.get(catAPIURL) as response:
+				result = await response.json()
+
+				catPictureURL = result["url"]
+				embed = discord.Embed(title="Meow")
+				embed.set_image(url=catPictureURL)
+				await ctx.send(embed=embed)
 
 
 @bot.command(name="roll", help="roll a dice in NdN format. 5d5")
-async def roll(ctx, dice: str):
-    """Rolls a dice in NdN format."""
-    result = utils._rollTheDice(dice)
-    await ctx.send(result)
+async def rollTheDice(ctx, dice: str):
+	"""Rolls a dice in NdN format."""
+	try:
+		rolls, limit = map(int, dice.split('d'))
+	except Exception:
+		return 'Format has to be in NdN!'
 
-
-@bot.command(name="weather", help="weather of world at your command")
-async def weather(ctx, *args):
-    """ Get Your City weather example:- !weather New Delhi"""
-    city_name = ' '.join(args)
-    openWeatherURL = f"http://api.openweathermap.org/data/2.5/forecast?q={city_name}&appid={WEATHER_API_KEY}"
-    
-    try:
-        weather_data = requests.get(openWeatherURL).json()
-
-        await ctx.send(f'{city_name.title()} - Country: {weather_data["city"]["country"]}')
-        await ctx.send(f'Temp: {round(weather_data["list"][0]["main"]["temp"] -273.0)}')
-        await ctx.send(f'temp_min: {round(weather_data["list"][0]["main"]["temp_min"] -273.0)}')
-        await ctx.send(f'temp_max: {round(weather_data["list"][0]["main"]["temp_max"] -273.0)}')
-        await ctx.send(f'pressure: {weather_data["list"][0]["main"]["pressure"]}')
-        await ctx.send(f'humidity: {weather_data["list"][0]["main"]["humidity"]}')
-        await ctx.send(f'sea_level:{weather_data["list"][0]["main"]["sea_level"]}')
-                        
-    except Exception:
-        await ctx.send("I didn't find your City")
+	result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+	await ctx.send(result)
 
 
 @bot.command(name="surl", help="shorten your url. use https/http")
-async def shortenURL(ctx, website: str):
-    """ Shorten Your Url using cleanURL API !surl <website> """
-    shortURL = utils._UrlShortner(website)
-    await ctx.send(f"Your Short URL is\n{shortURL}")
+async def getShortenURL(ctx, website: str):
+	""" Shorten Your Url using cleanURL API !surl <website> """
+	async with ctx.typing():
+		cleanAPIURL = 'https://cleanuri.com/api/v1/shorten'
+		async with aiohttp.ClientSession() as session:
+			async with session.post(cleanAPIURL, data={'url': website}) as session:
+				shortUrl = await response.json()['result_url']
+				await ctx.send(f"Your Short URL is\n{shortURL}")
 
 
 @bot.command(name="gh", help="get Github user data")
-async def githubAPI(ctx, username: str):
-    """ Get Github User Data Using !gh username """
-    userData = utils._getGitUserData(username)
-    await ctx.send(f'Name: {userData["name"]}\nPublic Repo: {userData["public_repos"]}\nFollowers: {userData["followers"]}\nLast Updated: {userData["updated_at"]}')
+async def getGihubUserData(ctx, username: str):
+	""" Get Github User Data Using !gh username """
+	async with ctx.typing():
+		gitAPIURL = f'https://api.github.com/users/{username}'
+		async with aiohttp.ClientSession() as session:
+			async with session.get(gitAPIURL) as response:
+				userData = await response.json()
+				await ctx.send(f'Name: {userData["name"]}\nPublic Repo: {userData["public_repos"]}\nFollowers: {userData["followers"]}\nLast Updated: {userData["updated_at"]}')
 
 
 @bot.command(name="ifsc", help="Get Indian Bank Branch details by IFSC Code")
 async def getBankDataByIFSC(ctx, ifsc_code: str):
-    """ Get Bank Details by IFSC CODE In INdia !ifsc <ifsc_code> """
-    bankData = utils._getIfscDetails(ifsc_code)
-    await ctx.send(f'Branch: {bankData["BRANCH"]}\nBank: {bankData["BANK"]}\nDistrict: {bankData["DISTRICT"]}\nState: {bankData["STATE"]}\nContact Number: {bankData["CONTACT"]}')
+	""" Get Bank Details by IFSC CODE In INdia !ifsc <ifsc_code> """
+	url = f"https://ifsc.razorpay.com/{ifscCode}"
+	async with ctx.typing():
+		async with aiohttp.ClientSession() as session:
+			async with session.get(url) as response:
+				bankData = await response.json()
+				await ctx.send(f'Branch: {bankData["BRANCH"]}\n\Bank: {bankData["BANK"]}\nDistrict: {bankData["DISTRICT"]}\nState: {bankData["STATE"]}\nContact Number: {bankData["CONTACT"]}')
 
 
-@bot.command(name="dogs", help="Get Random Pictures of Dogs")
-async def dogs(ctx):
-    """ Get Random Dogs Picture """
-    embed = utils._getRandomDogPicture()
-    await ctx.send(embed=embed)
+@bot.command(name="weather", help="weather of world at your command")
+async def getWeather(ctx, *args):
+	""" Get Your City weather example:- !weather New Delhi"""
+	city_name = ' '.join(args)
+	async with ctx.typing():
+		openWeatherURL = f"http://api.openweathermap.org/data/2.5/forecast?q={city_name}&appid={utils.WEATHER_API_KEY}"
+		async with aiohttp.ClientSession() as session:
+			async with session.get(openWeatherURL) as response:
+				if response.status == 200:
+					weather_data = await response.json()
+
+					await ctx.send(f'{city_name.title()} - Country: {weather_data["city"]["country"]}\n\
+									Temp: {round(weather_data["list"][0]["main"]["temp"] -273.0)}\n\
+									temp_min: {round(weather_data["list"][0]["main"]["temp_min"] -273.0)}\n\
+									temp_max: {round(weather_data["list"][0]["main"]["temp_max"] -273.0)}\n\
+									pressure: {weather_data["list"][0]["main"]["pressure"]}\n\
+									humidity: {weather_data["list"][0]["main"]["humidity"]}\n\
+									sea_level:{weather_data["list"][0]["main"]["sea_level"]}')
+				else:
+					await ctx.send(f"I can't find {city_name}")
 
 
-@bot.command(name="cats", help="Get Random Pictures of Cats")
-async def cats(ctx):
-    """ Get Random Cats Picture """
-    embed = utils._getRandomCatPicture()
-    await ctx.send(embed=embed)
+@bot.command(name="dogs", help="Get Random picture of dogs.")
+async def getRandomDogPicture():
+	pass
 
-
-""" Credits """
-bot_details = f'''BotName:- Cybel\n\
-Creator:- Deepak Raj\n\
-Contact Email:- Deepak008@live.com\n\
-Discord Server:- https://discord.gg/JfbK3bS\n\
-GitHub:- https://github.com/codeperfectplus'''
-
-
-@bot.command(name="info")
-async def bot_info(ctx):
-    """ Information about the Bot and credits """
-    await ctx.send(bot_details)
-    
 
 if __name__ == '__main__':
-    bot.run(TOKEN)
+	bot.run(utils.TOKEN)
