3f53573c5426ea9362f88d0bb004e075330d26f2
Deepak Raj <54245038+codePerfectPlus@users.noreply.github.com>
2022-10-17 15:25:26 +0530

    auto mod command disabled

--- src/cogs/nsfwCommands.py
+++ src/cogs/nsfwCommands.py
@@ -60,23 +60,23 @@ class NsfwCommands(commands.Cog, name="command for NSFW: NSFW Commands"):
 			await ctx.send(f'```{type(e).__name__} - {e}```')
 
 
-	@commands.Cog.listener()
-	async def on_message(self, message: discord.Message):
-		''' 
-		on_message event will fire when a message is sent.
+	# @commands.Cog.listener()
+	# async def on_message(self, message: discord.Message):
+	# 	''' 
+	# 	on_message event will fire when a message is sent.
 
-		Arguments:
-			message {discord.Message} -- The message object.
+	# 	Arguments:
+	# 		message {discord.Message} -- The message object.
 
-		'''
+	# 	'''
 
-		if message.author.bot:
-			return
-		elif message.author.id == self.bot.user.id:
-			return
-		elif profanity.contains_profanity(message.content): # delete the message if it contains profanity
-			await message.delete()
-			await message.channel.send(f"**{message.author.mention}**, **Please do not use bad words!**")
+	# 	if message.author.bot:
+	# 		return
+	# 	elif message.author.id == self.bot.user.id:
+	# 		return
+	# 	elif profanity.contains_profanity(message.content): # delete the message if it contains profanity
+	# 		await message.delete()
+	# 		await message.channel.send(f"**{message.author.mention}**, **Please do not use bad words!**")
 
 		
 def setup(bot: commands.Bot):
